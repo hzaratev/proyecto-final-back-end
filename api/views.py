562bbf80a823f7c2ee236860c2852afa74c4d2e8
Gonzalo <brutalo@icloud.com>
2019-05-28 12:44:03 -0400

    auth

--- api/views.py
+++ api/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.views import ObtainAuthToken
 
 # Create your views here.
 
@@ -17,3 +18,25 @@ class ContactView(APIView):
         serializer = ContactSerializer(contacts, many=True)
 
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+class CustomAuthToken(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'email': user.email
+            })
+
+class Logout(APIView):
+    def get(self, request, format=None):
+
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
+
